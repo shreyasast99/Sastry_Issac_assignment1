8e1f2e8f398de34a83b0ee4c2985cdab7cca6c4c
Shreya Sastry <srs93414@uga.edu>
2020-01-27 23:18:49 -0500

    added all files except main

---
+++ Instructor.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+bool login(string username, string password);
+string getInstructorName();
+Student getStudent(string username);
+Student getMinStudent(int gradeType);
+Student getMaxStudent(int gradeType);
+double getAvg(int gradeType);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Student.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+string fullName;
+int projectGrade;
+int quizGrade;
+int midtermGrade;
+int finalGrade;
+bool login(string username, string password);
+string getStudentName();
+int getProjectGrade();
+int getQuizGrade();
+int getMidtermGrade();
+int getFinalGrade();
+double getOverallGrade();
+/*
+ *Student constructor
+ */
+Student::Student(){}
+
+string getStudentName(){
+
+}
