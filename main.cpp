2a54363d27a864c3b53c4f379bd9c20c102f9017
Shreya Sastry <srs93414@uga.edu>
2020-01-29 17:09:05 -0500

    worked on main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,79 @@
 #include "Student.h"
 #include "Instructor.h"
 using namespace std;
-
+int person=0;
+string username;
+string pass;
+int query;
+string viewGrades;
 void main{
-    
+    while(person!=3){
+        cout<<"User types,\n 1 - Instructor\n 2 - Student\n select a login user type or enter 3 to exit:\n"<<endl;
+        cin>>person;
+        //checks if you are instructor(1) or student(2)
+        if(person==1){
+            //do instructor stuff
+            cout<<"Enter credentials to login, Enter username:\n"<<endl;
+            cin>>username;
+            cout<<"Enter password: \n"<<endl;
+            cin>>pass;
+            Instructor instructor(/*put param*/);
+            if(instructor.login(username,pass)==true){
+                cout<<"You are now logged in as instructor "<< instructor.getInstructorName()<< "."<<endl;
+                cout<<"Query options,\n1 - view grades of a student\n2 - view stats\nEnter option number:"<<endl;
+                cin>>query;
+                //checks the query option(view grades or view stats
+                while(query!=1&&query!=2){
+
+                }
+                if(query==1){
+                    cout<<"Enter student username to view grades: "<<endl;
+                }
+                else if(query==2){
+
+                }
+                else{
+                    cout<<"Invalid option. Please enter a valid option."<<endl;
+                }
+            }
+            else{
+                cout<<"Login as instructor failed"<<endl;
+            }
+        }
+        else if(person==2){
+            //do student stuff
+            cout<<"Enter credentials to login, Enter username:"<<endl;
+            cin>>username;
+            cout<<"Enter password: "<<endl;
+            cin>>pass;
+            Student student(/*put param*/);
+            student.login(username,pass);
+            //checks if login attempt is valid
+            if(student.login(username,pass)==true){
+                cout<<"You are now logged in as student "<< student.getStudentName()<< "."<<endl;
+                cout<<"Do you want to view grades (y/n)?"<<endl;
+                cin<<viewGrades;
+                if(viewGrades=="y"){
+                    cout<<"Student name: "<<student.getStudentName()<<endl;
+                    cout<<"Project"<<student.getProjectGrade()<<endl;
+                    cout<<"Quiz"<<student.getQuizGrade()<<endl;
+                    cout<<"Midterm"<<student.getMidtermGrade()<<endl;
+                    cout<<"Final"<<student.getFinalGrade()<<endl;
+                    cout<<"Overall"<<student.getOverallGrade()<<endl;
+                }
+                else{
+                    delete student;
+                }
+            }
+            else{
+                cout<<"Login as student failed."<<endl;
+            }
+        }
+        else{
+            cout<<"Invalid option. Please enter a valid option."<<endl;
+        }
+    }
+    if(person==3){
+        exit(0);
+    }
 }
