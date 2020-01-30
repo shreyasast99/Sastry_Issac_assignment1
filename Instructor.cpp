2a3a2352df0f7f9bc2aa3a46eb25be8f86c77bcb
Amanda Issac <ari31551@uga.edu>
2020-01-30 01:08:23 -0500

    added changes to instructor

--- Instructor.cpp
+++ Instructor.cpp
@@ -2,8 +2,11 @@
 #include<string>
 #include "Student.h"
 #include "Instructor.h"
+string username;
+string password;
 string firstName;
 string lastName;
+vector<Instructor> instructorVec;
 bool login(string username, string password);
 string getInstructorName();
 Student getStudent(string username);
@@ -12,11 +15,29 @@ Student getMaxStudent(int gradeType);
 double getAvg(int gradeType);
 
 //when the intructor logs in youshould make a instructor object with all instructor info. To check if they are actually an instructor then
-Instructor::Instructor(){
-
+Instructor::Instructor(string username, string password,string firstName, string lastName){
+  //setting the parsed values into the constructor to make the instructor object
+  this.username=username;
+  this.password=password;
+  this.firstName=firstName;
+  this.lastName=lastName;
 }
 bool Instructor::login(string username, string password){
-    return true;
+  //parse files
+  ifstream inInstructorFile("instructor.txt",ios::in);
+  
+  //checking if the ifstream could not open the file
+  if(!inInstructorFile){
+    cerr<<"File could not be opened"<<endl;
+    exit(1);
+  }//checking if file could be opened
+
+  while(inInstructorFile >> username >> password >> firstName >> lastName){
+    Instructor instructorTemp=new Instructor(); //creating instructor object
+    instructorTemp.login(username
+  }
+}
+return true;
 }
 string Instructor::getInstructorName(){
     return firstName+" "+lastName;
